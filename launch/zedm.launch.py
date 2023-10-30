94975933e0fbf9a6f5d2b8f58e61e31304f7ac6e
nimiCurtis <nimicu21@gmail.com>
2023-10-30 16:26:53 +0200

    test

--- launch/zedm.launch.py
+++ launch/zedm.launch.py
@@ -17,7 +17,6 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
-
 def generate_launch_description():
 
     # Camera model (force value)
